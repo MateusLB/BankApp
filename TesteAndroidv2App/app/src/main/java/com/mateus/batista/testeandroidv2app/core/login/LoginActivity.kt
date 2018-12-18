d4bafa8a0a94f7a0548a63fe828c6e9b0743cfbd
Mateus Batista <mateuslb91@gmail.com>
2018-12-17 23:34:41 -0200

    - Material Component

--- TesteAndroidv2App/app/src/main/java/com/mateus/batista/testeandroidv2app/core/login/LoginActivity.kt
+++ TesteAndroidv2App/app/src/main/java/com/mateus/batista/testeandroidv2app/core/login/LoginActivity.kt
@@ -1,17 +1,23 @@
 package com.mateus.batista.testeandroidv2app.core.login
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.mateus.batista.testeandroidv2app.R
 import com.mateus.batista.testeandroidv2app.base.view.BaseActivity
+import com.mateus.batista.testeandroidv2app.databinding.ActivityLoginBinding
 import com.mateus.batista.testeandroidv2app.utils.viewModel
 
 class LoginActivity : BaseActivity() {
 
     private lateinit var loginViewModel: LoginViewModel
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         loginViewModel = viewModel(viewModelFactory)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.viewModel = loginViewModel
+        binding.executePendingBindings()
     }
 }
